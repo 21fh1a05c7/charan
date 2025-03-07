763a2aa5fece79891d588e6eb749ec648ffe31da
YEpafra20 <yepafra1@gmail.com>
2025-03-07 19:41:56 +0530

    adding new files

---
+++ OneDrive/Desktop/python.py/string.py
@@ -0,0 +1,17 @@
+a="python life"#string mehods
+print(a.upper())
+b="PYTHON LIFE"
+print(b.lower())
+ch="charan"
+print(ch.count('r'))
+
+pythonlife=" python language  "
+#print(pythonlife.removeprefix('python'))
+#print(pythonlife.removesuffix("language"))
+#print(pythonlife.startswith('p'))
+#print(pythonlife.endswith('language'))
+#print(pythonlife.split())#convret in string
+print(pythonlife)
+print(len(pythonlife))
+print(pythonlife.strip())
+print(len(pythonlife))
